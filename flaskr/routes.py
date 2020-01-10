f0dabd59d7f81938283710bbb01ce350631cd54f
Pythondev <pythondev@ongraph.com>
2020-01-10 15:26:54 +0530

    [Add] New function to return time a/c timezone

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,6 +1,6 @@
 from flaskr import app
-from datetime import datetime
-
+import datetime
+import pytz
 
 @app.route('/')
 def hello():
@@ -9,6 +9,16 @@ def hello():
 
 @app.route('/ctime')
 def utc_time():
-    date_time = datetime.strftime(datetime.utcnow(), "%m/%d/%Y %H:%M:%S")
+    current_time = datetime.datetime.now(tz=pytz.utc)
+    date_time = current_time.strftime("%m/%d/%Y %H:%M:%S")
     return "The UTC time with format 'mm/dd/YYYY HH:MM:SS' is {}".format(date_time)
 
+@app.route('/ctime/<path:timeZone>')
+def utc_time_zone(timeZone):
+    try:
+        tz = pytz.timezone(timeZone)
+        current_time = datetime.datetime.now(tz=tz).strftime("%m/%d/%Y %H:%M:%S")
+        response = current_time
+    except pytz.exceptions.UnknownTimeZoneError:
+        response = "Unknown time zone please provide valid timezone"
+    return response
