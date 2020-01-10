44549b8042ce75102ef4ec397459f3d09604cbe7
Pythondev <pythondev@ongraph.com>
2020-01-10 15:24:39 +0530

    [update] add connexion configuration to flask app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+import connexion
 
-app = Flask(__name__)
+# app = Flask(__name__)
+# app = connexion.FlaskApp(__name__, specification_dir='../openapi/')
+app = connexion.App(__name__,specification_dir='../openapi/', debug=True, host='0.0.0.0')
+app.add_api('swagger.yaml')
+app = app.app
 
 from flaskr import routes
